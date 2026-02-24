3b81d1631fed2d5da7c639eb82ee469c477ba3ef
Bilal Saeed <mianzainjh@gmail.com>
2026-02-24 16:20:52 +0500

    Today I fetch posts apis.

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+
+import 'model/posts_model.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,18 +14,47 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
 
+  List<PostModel> postList = [];
 
-
+  Future<List<PostModel>> getPostApi () async{
+    final response = await  http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    var data = jsonDecode(response.body.toString());
+    if(response.statusCode == 200){
+      for (Map i in data) {
+        postList.add(PostModel.fromJson(i as Map<String, dynamic>));
+      }
+      return postList;
+    }else{
+      return postList;
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blue,
         title: Text("Api Testing "),
+        centerTitle: true,
 
       ),
       body: Column(
         children: [
-          Text("Api Testing")
+          Expanded(
+            child: FutureBuilder(
+              future: getPostApi(),
+              builder: (context, snapshot){
+                if(!snapshot.hasData){
+                  return CircularProgressIndicator();
+                }else{
+                  return ListView.builder(
+                      itemCount: postList.length,
+                      itemBuilder: (context, index){
+                        return Text(postList[index].title.toString());
+                      });
+                }
+              },
+            ),
+          )
         ],
       ),
     );
