2b4e6dce6c2f79bb3e9b62ff8db144d367da2264
Bilal Saeed <mianzainjh@gmail.com>
2026-03-12 16:25:48 +0500

    Today I learned how to fetch Photos data..and set Photos Model.

--- lib/model/photos_model.dart
+++ lib/model/photos_model.dart
@@ -13,23 +13,5 @@ class Photos {
     this.thumbnailUrl,
   });
 
-  // JSON se object banane ke liye
-  Photos.fromJson(Map<String, dynamic> json) {
-    albumId = json['albumId'];
-    id = json['id'];
-    title = json['title'];
-    url = json['url'];
-    thumbnailUrl = json['thumbnailUrl'];
-  }
 
-  // Object ko JSON me convert karne ke liye
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = {};
-    data['albumId'] = albumId;
-    data['id'] = id;
-    data['title'] = title;
-    data['url'] = url;
-    data['thumbnailUrl'] = thumbnailUrl;
-    return data;
-  }
 }
