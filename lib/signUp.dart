d6615fe23edd7c0a6dfa80154111c70040a86933
Bilal Saeed <mianzainjh@gmail.com>
2026-03-15 10:43:30 +0500

    Today I learned about signup API.

--- lib/signUp.dart
+++ lib/signUp.dart
@@ -1,6 +1,7 @@
-
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 
 class Signup extends StatefulWidget {
   const Signup({super.key});
@@ -14,16 +15,41 @@ class _SignupState extends State<Signup> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
-  void login(String email, String password){
+  void login(String email, String password) async {
 
-    try{
+    try {
 
-    }catch(e){
-      e.toString();
+      http.Response response = await http.post(
+        Uri.parse('https://reqres.in/api/register'),
+        body: {
+          'email': email,
+          'password': password,
+        },
+      );
+
+      print(response.statusCode);
+      print(response.body);
+
+      if(response.statusCode == 200){
+        Get.snackbar(
+          "Success",
+          "Account Created Successfully",
+          backgroundColor: Colors.green,
+          colorText: Colors.white,
+        );
+      }else{
+        Get.snackbar(
+          "Error",
+          "Signup Failed",
+          backgroundColor: Colors.red,
+          colorText: Colors.white,
+        );
+      }
+
+    } catch (e) {
+      print(e);
     }
   }
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -31,50 +57,57 @@ class _SignupState extends State<Signup> {
       appBar: AppBar(
         centerTitle: true,
         backgroundColor: Colors.blue,
-        title: Text("Sign Up Api"),
+        title: const Text("Sign Up Api"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: Column(
           children: [
-            TextFormField(
 
+            TextFormField(
               controller: emailController,
               decoration: InputDecoration(
-
                 border: OutlineInputBorder(
-
-                  borderRadius: BorderRadius.circular(15)
+                    borderRadius: BorderRadius.circular(15)
                 ),
-                  hintText: "Email",
+                hintText: "Email",
               ),
             ),
-            Gap(10),
-            TextFormField(
 
+            const Gap(10),
+
+            TextFormField(
               controller: passwordController,
               decoration: InputDecoration(
                 border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15)
+                    borderRadius: BorderRadius.circular(15)
                 ),
-
                 hintText: "Password",
               ),
             ),
-            Gap(15),
+
+            const Gap(15),
+
             InkWell(
               onTap: (){
-                login(emailController.text.toString(), passwordController.text.toString());
+                login(
+                  emailController.text,
+                  passwordController.text,
+                );
               },
               child: Container(
-                height: 30,
+                height: 45,
                 width: double.infinity,
-
                 decoration: BoxDecoration(
                   color: Colors.green,
                   borderRadius: BorderRadius.circular(20),
                 ),
-                child: Center(child: Text("Sign Up",style: TextStyle(color: Colors.white),)),
+                child: const Center(
+                  child: Text(
+                    "Sign Up",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
               ),
             )
           ],
